c33fd847a72cdcd702778844ecf1efa01191d3e2
Max Goudy <goudymt@gmail.com>
2026-02-01 20:12:57 -0800

    Reduced 26 columns down to 13: price, year, manufacturere, model, condition, cylinders, fuel, odometer, title_status, transmission, drive, type, and state.

--- data-preperation/clean_vehicles.py
+++ data-preperation/clean_vehicles.py
@@ -1,4 +1,5 @@
 # Craigslist used cars data cleaning script
+# https://www.kaggle.com/datasets/austinreese/craigslist-carstrucks-data
 
 # This script cleans the raw Craiglist vehicles dataset and prepares it for use across all four ML frameworks.
 
@@ -31,3 +32,41 @@ print(f"Raw dataset shape: {df.shape}")
 # convers the column names from a pandas index to a list
 # shows us all available columns in the dataset
 print(f"Columns: {df.columns.tolist()}")
+
+# Step 2: selecting relevant columns
+
+# We won't need all 25 columns from the raw data
+# Selecting only the columns useful for predicting car prices removes unwanted variables
+# This reduces memory usage and simplifies our analysis
+
+# When running the code from step 1, we recieved:
+# Raw dataset shape: (426880, 26)
+# Columns: ['id', 'url', 'region', 'region_url', 'price', 'year', 'manufacturer', 'model', 'condition', 'cylinders', 'fuel',
+#  'odometer', 'title_status', 'transmission', 'VIN', 'drive', 'size', 'type', 'paint_color', 'image_url', 'description',
+#  'county', 'state', 'lat', 'long', 'posting_date']
+
+# List of columns names we want to keep for our model:
+COLUMNS_TO_KEEP = [
+    'price',            # Target variable - what we want to predict
+    'year',             # Numeric - model year of the vehicle
+    'manufacturer',     # Categorical - brand name (ford, toyota, ect.) 
+    'model',            # Categorical - specific model name
+    'condition',        # Categorical - excellent, good, fair, ect.
+    'cylinders',        # Categorical - engine cylinders (4, 6, 8)
+    'fuel',             # Categorical - gas, diesal, hybrid, electric
+    'odometer',         # Numeric - miles driven  
+    'title_status',     # Categorical - clean, salvage, rebuilt
+    'transmission',     # Categorical - automatic, manual, other
+    'drive',            # Categorical - fwd, rwd, 4wd
+    'type',             # Categorical - Sedan, SUV, truck, coupe
+    'state'             # Categorical - US state where listed
+]
+
+# Using double brackers to select multiple columns from the dataframe
+# .copy() creates an independent copy so changes don't affect original df
+df = df[COLUMNS_TO_KEEP].copy()
+
+# verify our column selection worked
+print(f"\nAfter column selection: {df.shape}")      
+# After column selection: (426880, 13)
+

35672dead2f215bf42c162e7de2a9db3a6f2aa62
Max Goudy <goudymt@gmail.com>
2026-02-01 20:23:14 -0800

    initial data exploration, looking for isnull and column statistics.

--- data-preperation/clean_vehicles.py
+++ data-preperation/clean_vehicles.py
@@ -70,3 +70,72 @@ df = df[COLUMNS_TO_KEEP].copy()
 print(f"\nAfter column selection: {df.shape}")      
 # After column selection: (426880, 13)
 
+# Step 3: Initial data exploration
+
+# Before cleaning, we want to understand what we're working with
+
+# df.info prints:
+# - Column names
+# - Non-null counts (help identify missing data)
+# - Data types (int, float, object/string)
+print("\n--- Data Types amd Non-Null Counts ---")
+print(df.info())
+
+"""
+RangeIndex: 426880 entries, 0 to 426879
+Data columns (total 13 columns):
+ #   Column        Non-Null Count   Dtype  
+---  ------        --------------   -----
+ 0   price         426880 non-null  int64
+ 1   year          425675 non-null  float64
+ 2   manufacturer  409234 non-null  str
+ 3   model         421603 non-null  str
+ 4   condition     252776 non-null  str
+ 5   cylinders     249202 non-null  str
+ 6   fuel          423867 non-null  str
+ 7   odometer      422480 non-null  float64
+ 8   title_status  418638 non-null  str
+ 9   transmission  424324 non-null  str
+ 10  drive         296313 non-null  str
+ 11  type          334022 non-null  str
+ 12  state         426880 non-null  str
+"""
+
+# df.isnull().sum() counts missing values in each column
+# .isnull() creates a True/False mask where True = missing
+# .sum() adds up the Trues 
+print("\n--- Missing Values Per Column ---")
+print(df.isnull().sum())
+
+"""
+price                0
+year              1205
+manufacturer     17646
+model             5277
+condition       174104
+cylinders       177678
+fuel              3013
+odometer          4400
+title_status      8242
+transmission      2556
+drive           130567
+type             92858
+state                0
+"""
+
+# df.describe() shows statistics for numeric columns
+# This helps identify outliers (price of 1$ or $1,000,000 which skew the data)
+print("\n--- Numeric Column Statistics ---")
+print(df.describe())
+
+"""
+              price           year      odometer
+count  4.268800e+05  425675.000000  4.224800e+05
+mean   7.519903e+04    2011.235191  9.804333e+04
+std    1.218228e+07       9.452120  2.138815e+05
+min    0.000000e+00    1900.000000  0.000000e+00
+25%    5.900000e+03    2008.000000  3.770400e+04
+50%    1.395000e+04    2013.000000  8.554800e+04
+75%    2.648575e+04    2017.000000  1.335425e+05
+max    3.736929e+09    2022.000000  1.000000e+07
+"""
